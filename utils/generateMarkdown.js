c46a78f9ffe11249b2877d4ec5cf77f38cfb5159
choonchy <57583113+choonchy@users.noreply.github.com>
2021-05-10 00:16:38 +1000

    Changes to code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,7 +14,62 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contribution](#contribution)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  Running the following command will install the required dependencies of this project:
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  This project is running using the ${data.license} license
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Tests
+
+  You can run tests using the following command:
+
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+
+  ## Questions
+
+  If you have any questions about the project, you should contact ${data.username} using the following methods:
+
+  [GitHub]]{https://github.com/${data.username}}
+  [Email]{${data.email}}
+  
+
+  ### This README file was generated using Tom Chappell's [README Generator]{https://github.com/choonchy/week-9-homework}
+  `;
 }
 
 module.exports = generateMarkdown;
